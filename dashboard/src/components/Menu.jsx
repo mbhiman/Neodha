a3303c0e71ed2c6a9359285f898ae538e2ed792f
Bhiman Singha <mbhiman70@gmail.com>
2025-05-01 20:56:42 +0530

    Added dynamic calculations with dummy data to Holdings & Positions

--- dashboard/src/components/Menu.jsx
+++ dashboard/src/components/Menu.jsx
@@ -24,34 +24,34 @@ function Menu() {
             <div className="menus">
                 <ul>
                     <li>
-                        <Link to={'/'} onClick={() => handleMenuClick(0)} style={{textDecoration:'none'}}>
+                        <Link to={'/'} onClick={() => handleMenuClick(0)} style={{ textDecoration: 'none' }}>
                             <p className={selectedMenu === 0 ? activeMenuClass : menuClass}>Dashboard</p>
                         </Link>
                     </li>
                     <li>
-                        <Link to={'/orders'} onClick={() => handleMenuClick(1)} style={{textDecoration:'none'}}>
+                        <Link to={'/orders'} onClick={() => handleMenuClick(1)} style={{ textDecoration: 'none' }}>
                             <p className={selectedMenu === 1 ? activeMenuClass : menuClass}>Orders</p>
                         </Link>
                     </li>
 
                     <li>
-                        <Link to={'/holdings'} onClick={() => handleMenuClick(2)} style={{textDecoration:'none'}}>
+                        <Link to={'/holdings'} onClick={() => handleMenuClick(2)} style={{ textDecoration: 'none' }}>
                             <p className={selectedMenu === 2 ? activeMenuClass : menuClass}>Holdings</p>
                         </Link>
                     </li>
 
                     <li>
-                        <Link to={'/positions'} onClick={() => handleMenuClick(3)} style={{textDecoration:'none'}}>
+                        <Link to={'/positions'} onClick={() => handleMenuClick(3)} style={{ textDecoration: 'none' }}>
                             <p className={selectedMenu === 3 ? activeMenuClass : menuClass}>Positions</p>
                         </Link>
                     </li>
                     <li>
-                        <Link to={'/funds'} onClick={() => handleMenuClick(4)} style={{textDecoration:'none'}}>
+                        <Link to={'/funds'} onClick={() => handleMenuClick(4)} style={{ textDecoration: 'none' }}>
                             <p className={selectedMenu === 4 ? activeMenuClass : menuClass}>Funds</p>
                         </Link>
                     </li>
                     <li>
-                        <Link to={'/apps'} onClick={() => handleMenuClick(5)} style={{textDecoration:'none'}}>
+                        <Link to={'/apps'} onClick={() => handleMenuClick(5)} style={{ textDecoration: 'none' }}>
                             <p className={selectedMenu === 5 ? activeMenuClass : menuClass}>Apps</p>
                         </Link>
                     </li>
