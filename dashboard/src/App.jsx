8b856a4619addf8d62cfdcb167166f0f5803c2fc
Bhiman Singha <mbhiman70@gmail.com>
2025-05-01 10:58:39 +0530

    Developed the inner routing for dashboard

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <BrowserRouter >
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/*" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
--- dashboard/src/components/Menu.jsx
+++ dashboard/src/components/Menu.jsx
@@ -1,21 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import kiteLogo from '../assets/images/kiteLogo.png';
 
 function Menu() {
+
+    const [selectedMenu, setSelectedMenu] = useState(0);
+    const [isProfileDropdownOpen, setIsProfileDropdownOpen] = useState(false);
+
+    const handleMenuClick = (i) => {
+        setSelectedMenu(i);
+    }
+
+    const handleProfileClick = (i) => {
+        setIsProfileDropdownOpen(!isProfileDropdownOpen);
+    }
+
+    const menuClass = 'menu';
+    const activeMenuClass = 'menu selected';
+
     return (
         <div className="menu-container">
             <img src={kiteLogo} style={{ width: "50px" }} />
             <div className="menus">
                 <ul>
-                    <li><p>Dashboard</p></li>
-                    <li><p>Orders</p></li>
-                    <li><p>Holdings</p></li>
-                    <li><p>Positions</p></li>
-                    <li><p>Funds</p></li>
-                    <li><p>Apps</p></li>
+                    <li>
+                        <Link to={'/'} onClick={() => handleMenuClick(0)} style={{textDecoration:'none'}}>
+                            <p className={selectedMenu === 0 ? activeMenuClass : menuClass}>Dashboard</p>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={'/orders'} onClick={() => handleMenuClick(1)} style={{textDecoration:'none'}}>
+                            <p className={selectedMenu === 1 ? activeMenuClass : menuClass}>Orders</p>
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link to={'/holdings'} onClick={() => handleMenuClick(2)} style={{textDecoration:'none'}}>
+                            <p className={selectedMenu === 2 ? activeMenuClass : menuClass}>Holdings</p>
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link to={'/positions'} onClick={() => handleMenuClick(3)} style={{textDecoration:'none'}}>
+                            <p className={selectedMenu === 3 ? activeMenuClass : menuClass}>Positions</p>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={'/funds'} onClick={() => handleMenuClick(4)} style={{textDecoration:'none'}}>
+                            <p className={selectedMenu === 4 ? activeMenuClass : menuClass}>Funds</p>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={'/apps'} onClick={() => handleMenuClick(5)} style={{textDecoration:'none'}}>
+                            <p className={selectedMenu === 5 ? activeMenuClass : menuClass}>Apps</p>
+                        </Link>
+                    </li>
+
                 </ul>
                 <hr />
-                <div className="profile">
+                <div className="profile" onClick={handleProfileClick}>
                     <div className="avatar">AS</div>
                     <p className="username">USERID</p>
                 </div>
